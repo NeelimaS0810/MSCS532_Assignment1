1160ffe2af1dcf8c9d8b74217e99af089d957668
nsivala49401@ucumberlands.edu <nsivala49401@ucumberlands.edu>
2026-01-18 17:35:10 -0500

    added-the-test-data

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,3 +6,12 @@ def insertion_sort_decreasing(A):
             A[i + 1] = A[i]
             i = i - 1
         A[i + 1] = key
+
+if __name__ == "__main__":
+    # Test data
+test_array = [5, 2, 4, 6, 1, 3]
+print("Original Array:", test_array)
+ # Execute the sort [cite: 28]
+insertion_sort_decreasing(test_array)  
+# Verify the output 
+print("Sorted Array (Decreasing):", test_array)
